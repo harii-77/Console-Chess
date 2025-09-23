7ba408bc9104e230301c4252c2ae9598c5f7de1f
Hariharan Murugan <hariharanvj7777@gmail.com>
2025-09-23 11:50:01 +0530

    Add run.bat for one-command compile and run on Windows

---
+++ run.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+set SRC=src\main\java
+set OUT=target\classes
+
+if not exist "%OUT%" mkdir "%OUT%"
+echo Compiling Java sources...
+javac -d "%OUT%" -cp "%OUT%" "%SRC%\com\consolechess\PieceColor.java" "%SRC%\com\consolechess\PieceType.java" "%SRC%\com\consolechess\Piece.java" "%SRC%\com\consolechess\Position.java" "%SRC%\com\consolechess\Player.java" "%SRC%\com\consolechess\Board.java" "%SRC%\com\consolechess\ChessGame.java"
+if errorlevel 1 (
+  echo.
+  echo Build failed. Fix the errors above.
+  exit /b 1
+)
+
+echo.
+echo Starting Console Chess...
+java -cp "%OUT%" com.consolechess.ChessGame
