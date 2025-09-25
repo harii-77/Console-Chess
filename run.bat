88d6d3f0a9931e78f9f6e203ff7a2fb757a6d30c
Hariharan Murugan <hariharanvj7777@gmail.com>
2025-09-25 09:10:26 +0530

    Update run.bat to include MoveLogger in compilation
    - Add MoveLogger.java to javac compilation command
    - Ensures MoveLogger is compiled when running the game
    - Maintains proper compilation order for dependencies

--- run.bat
+++ run.bat
@@ -5,7 +5,7 @@ set OUT=target\classes
 
 if not exist "%OUT%" mkdir "%OUT%"
 echo Compiling Java sources...
-javac -d "%OUT%" -cp "%OUT%" "%SRC%\com\consolechess\PieceColor.java" "%SRC%\com\consolechess\PieceType.java" "%SRC%\com\consolechess\Piece.java" "%SRC%\com\consolechess\Position.java" "%SRC%\com\consolechess\Player.java" "%SRC%\com\consolechess\Board.java" "%SRC%\com\consolechess\ChessGame.java"
+javac -d "%OUT%" -cp "%OUT%" "%SRC%\com\consolechess\PieceColor.java" "%SRC%\com\consolechess\PieceType.java" "%SRC%\com\consolechess\Piece.java" "%SRC%\com\consolechess\Position.java" "%SRC%\com\consolechess\Player.java" "%SRC%\com\consolechess\MoveLogger.java" "%SRC%\com\consolechess\Board.java" "%SRC%\com\consolechess\ChessGame.java"
 if errorlevel 1 (
   echo.
   echo Build failed. Fix the errors above.
